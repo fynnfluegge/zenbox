9d2fb94f72a0d8c8c99982656191b47541317f07
fynnfluegge <fynnfluegge@gmx.de>
2023-11-10 16:23:41 +0100

    fix firefox bookmarks parsing

--- .scripts/fzfbookmarks.sh
+++ .scripts/fzfbookmarks.sh
@@ -26,7 +26,7 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
 fi
 
 # if bookmarks file does not exist, use firefox bookmarks
-# if [ ! -f "$BOOKMARKS_FILE" ]; then
+if [ ! -f "$BOOKMARKS_FILE" ]; then
   USE_CHROME=false
   if [ "$(uname)" == "Darwin" ]; then
     BOOKMARKS_FILE="${HOME}/Library/Application Support/Firefox/Profiles/$(ls ${HOME}/Library/Application\ Support/Firefox/Profiles/ | grep default-release)/places.sqlite"
@@ -37,7 +37,7 @@ fi
   elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     BOOKMARKS_FILE="/${HOME}AppData/Roaming/Mozilla/Firefox/Profiles/$(ls /${HOME}AppData/Roaming/Mozilla/Firefox/Profiles/ | grep default-release)/places.sqlite"
   fi
-# fi
+fi
 
 if [ ! -f "$BOOKMARKS_FILE" ]; then
   echo "Bookmarks file not found. Currently only supports Google Chrome and Firefox."
@@ -49,17 +49,15 @@ if [ "$USE_CHROME" = true ]; then
   bookmark_titles_0=$(jq '[.roots.bookmark_bar.children[] | select(.type == "url") | .name]' "$BOOKMARKS_FILE")
   bookmark_titles_1=$(jq '[.roots.bookmark_bar.children[] | select(.children) | .children[] | select(.type == "url") | .name]' "$BOOKMARKS_FILE")
   bookmark_titles_2=$(jq '[.roots.bookmark_bar.children[] | select(.children) | .children[] | select(.children) | .children[] | select(.type == "url") | .name]' "$BOOKMARKS_FILE")
+  bookmark_titles="$bookmark_titles_0 $bookmark_titles_1 $bookmark_titles_2"
   bookmark_urls_0=$(jq '[.roots.bookmark_bar.children[] | select(.type == "url") | .url]' "$BOOKMARKS_FILE")
   bookmark_urls_1=$(jq '[.roots.bookmark_bar.children[] | select(.children) | .children[] | select(.type == "url") | .url]' "$BOOKMARKS_FILE")
   bookmark_urls_2=$(jq '[.roots.bookmark_bar.children[] | select(.children) | .children[] | select(.children) | .children[] | select(.type == "url") | .url]' "$BOOKMARKS_FILE")
+  bookmark_urls="$bookmark_urls_0 $bookmark_urls_1 $bookmark_urls_2"
 else
-  echo "Using Firefox bookmarks"
-output_file="bookmarks.json"
-temp_bookmarks_file="temp_bookmarks.sqlite"
-cp "$BOOKMARKS_FILE" "$temp_bookmarks_file"
-
-echo $BOOKMARKS_FILE
-echo $temp_bookmarks_file
+  output_file="bookmarks.json"
+  temp_bookmarks_file="temp_bookmarks.sqlite"
+  cp "$BOOKMARKS_FILE" "$temp_bookmarks_file"
 
 sqlite3 "$temp_bookmarks_file" <<EOF > "$output_file"
 .headers on
@@ -71,51 +69,49 @@ JOIN moz_bookmarks b
 ON h.id = b.fk;
 EOF
 
-echo $output_file
+  bookmark_titles_0=($(jq '.[].title' bookmarks.json))
+  bookmark_urls_0=($(jq '.[].url' bookmarks.json))
 
-# Print the lists
-bookmark_titles_0=($(jq '.[].title' bookmarks.json))
-bookmark_urls_0=($(jq '.[].url' bookmarks.json))
+  # Merge elements between double quotes
+  merged_titles=()
+  current_title=""
 
-# Merge elements between double quotes
-merged_titles=()
-current_title=""
+  for title in "${bookmark_titles_0[@]}"; do
+    if [[ $title == \"*\" ]]; then
+      # Title starts and ends with a double quote
+      merged_titles+=("${title}")
+    elif [[ $title == \"* ]]; then
+      # Title starts with a double quote
+      current_title="${title}"
+    elif [[ $title == *\" ]]; then
+      # Title ends with a double quote
+      current_title="${current_title} $title"
+      merged_titles+=("${current_title}")
+      current_title=""
+    else
+      # Title without double quotes
+      current_title="${current_title} $title"
+    fi
+  done
 
-for title in "${bookmark_titles_0[@]}"; do
-  if [[ $title == \"* ]]; then
-    # Title starts with a double quote
-    current_title="${title}"
-  elif [[ $title == *\" ]]; then
-    # Title ends with a double quote
-    current_title="${current_title} $title"
-    merged_titles+=("${current_title}")
-    current_title=""
-  else
-    # Title without double quotes
-    current_title="${current_title} $title"
-  fi
-done
+  # Join array elements with newlines
+  bookmark_titles=$(printf "%s\n" "${merged_titles[@]}")
+  bookmark_urls=$(printf "%s\n" "${bookmark_urls_0[@]}")
 
-# Print the merged list
-# Join array elements with newlines
-bookmark_titles_0=$(printf "%s\n" "${merged_titles[@]}")
-bookmark_urls_0=$(printf "%s\n" "${bookmark_urls_0[@]}")
-echo "Titles: ${bookmark_titles_0[@]}"
-echo "URLs: ${bookmark_urls_0[@]}"
-
-find . -name "temp_bookmarks.*" -exec rm {} \;
+  find . -name "temp_bookmarks.*" -exec rm {} \;
 fi
 
-bookmark_matches="$(echo "$bookmark_titles_0" | grep -o '"[^"]\+"' || echo "$bookmark_titles_0" | grep -o '[^, ]\+')
+# Split the double quoted elements string into an array
+bookmark_matches="$(echo "$bookmark_titles" | grep -o '"[^"]\+"' || echo "$bookmark_titles" | grep -o '[^, ]\+')
 "
-url_matches="$(echo "$bookmark_urls_0" | grep -o '"[^"]\+"' || echo "$bookmark_urls_0" | grep -o '[^, ]\+')
+url_matches="$(echo "$bookmark_urls" | grep -o '"[^"]\+"' || echo "$bookmark_urls" | grep -o '[^, ]\+')
 "
 
 # Use sed to remove double quotes and any leading/trailing spaces
 bookmark_result=$(echo "$bookmark_matches" | sed 's/"//g; s/^ *//; s/ *$//')
 url_result=$(echo "$url_matches" | sed 's/"//g; s/^ *//; s/ *$//')
 
-# # Use fzf for fuzzy searching
+# Use fzf for fuzzy searching
 selected_title=$(printf "%s\n" "${bookmark_result[@]}" | fzf --preview "open {}" --preview-window=up:0%:wrap)
 
 # Convert the formatted string to an array
